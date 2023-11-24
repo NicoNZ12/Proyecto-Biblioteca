3d95c02e2b5c7d82e39a1e1a5f4bab3fad714cc8
Nicolas Nuñez <nuneznicolas582@gmail.com>
2023-11-24 12:27:08 -0300

    Modifico la clase Usuario y la clase Main

---
+++ Act2Neoris/src/biblioteca/Main.java
@@ -0,0 +1,16 @@
+package biblioteca;
+
+import biblioteca.Libro;
+import claseabstracta.Registro;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import usuarioregistro.Usuario;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Usuario nuevoUsuario = new Usuario();
+        nuevoUsuario.registrarUsuario();
+    }
+}
--- Act2Neoris/src/usuarioregistro/Usuario.java
+++ Act2Neoris/src/usuarioregistro/Usuario.java
@@ -3,6 +3,9 @@ package usuarioregistro;
 
 import biblioteca.Libro;
 import claseabstracta.Registro;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Usuario extends Registro {
     private Libro libros;
@@ -15,4 +18,54 @@ public class Usuario extends Registro {
         super(usuario, contraseña);
     }
     
+    public void registrarUsuario() {
+        List<Usuario> usuarios = new ArrayList<>();
+        int opc = 0;
+        do {
+            System.out.println("--MENU PRINCIPAL--");
+            System.out.println("1.Registrarse\n2.Iniciar Sesion\n0.Salir");
+            Scanner sc = new Scanner(System.in);
+            opc = Integer.parseInt(sc.nextLine());
+
+            switch (opc) {
+                case 1:
+                    System.out.println("\nIngrese su usuario: ");
+                    String usuario = sc.nextLine();
+                    while (usuario.equals("")) {
+                        System.out.println("El usuario no puede estar en blanco");
+                        usuario = sc.nextLine();
+                    }
+
+                    System.out.println("Ingrese una contrasenia(8 caracteres): ");
+                    String contrasenia = sc.nextLine();
+                    while (contrasenia.length() != 8) {
+                        System.out.println("Ingrese una contrasenia de 8 caracteres");
+                        contrasenia = sc.nextLine();
+                    }
+                    Usuario nuevoUsuario = new Usuario(usuario, contrasenia);
+                    usuarios.add(nuevoUsuario);
+                    System.out.println("\n*****Usuario: " + "\"" + nuevoUsuario.getUsuario() + "\" registrado correctamente*****\n");
+                    break;
+
+                case 2:
+                    System.out.println("\nUsuario: ");
+                    usuario = sc.nextLine();
+                    System.out.println("Contrasenia: ");
+                    contrasenia = sc.nextLine();
+                    Usuario inicioSesion = new Usuario(usuario, contrasenia);
+                    if (usuarios.contains(inicioSesion)) {
+                        System.out.println("\nBIENVENIDO "+ inicioSesion.getUsuario() + "!\n");
+                        new Libro().mostrarMenu();
+                    } else {
+                        System.out.println("\n*****Usuario NO registrado*****\n");
+                    }
+                    break;
+                case 0:
+                    System.out.println("\nGracias por usar la aplicacion.\nDesarrollador: Nicolas Nunez.");
+                    break;
+                default:
+                    System.out.println("Opcion invalida. Ingrese nuevamente\n");
+            }
+        } while (opc != 0);
+    }
 }
